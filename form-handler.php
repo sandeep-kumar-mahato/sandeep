3c4025d22711bcc8c4168468aaed2a91c9a14abc
Sandeep <sandeepkumarmahato712@gmail.com>
2022-06-13 09:50:40 +0530

    init

--- form-handler.php
+++ form-handler.php
@@ -15,7 +15,7 @@ $to = 'sandeepkumarmahato712@gmail.com';
 
 $headers = "From: $email_from \r\n";
 
-$headers = "Reply-To: $visitor_email \r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
 
 mail($to,$email_subject,$email_body,$headers);
 
